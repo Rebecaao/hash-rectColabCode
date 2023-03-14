04938649b8e60bef7ba0e2fa5662c785563c32f0
Rebeca Lopes <rebecasunaika@gmail.com>
2023-03-14 11:05:47 -0300

    comecando arquitetura de pastas, usando itcc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/settings/color.css'
+import './styles/generic/reset.css'
+import'./styles/elements/bases.css'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
